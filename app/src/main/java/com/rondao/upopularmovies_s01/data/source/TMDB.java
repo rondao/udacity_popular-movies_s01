5bf87321d7724ca4f7cf8d4a2fa0b3238a59dd11
Rafael Rondão <rafael.rondao@gmail.com>
2018-02-13 20:59:09 -0200

    Adding class to store API Key for TMDB.
    Also, make git consider it always unchanged.
    So we can set the real API Key without adding it to git.
    
    git update-index --assume-unchanged <filepath>
    
    Signed-off-by: Rafael Rondão <rafael.rondao@gmail.com>

---
+++ app/src/main/java/com/rondao/upopularmovies_s01/data/source/TMDB.java
@@ -0,0 +1,9 @@
+package com.rondao.upopularmovies_s01.data.source;
+
+public class TMDB {
+    private static final String API_KEY = "YOUR TMDB API KEY";
+
+    public static String getKey() {
+        return API_KEY;
+    }
+}
