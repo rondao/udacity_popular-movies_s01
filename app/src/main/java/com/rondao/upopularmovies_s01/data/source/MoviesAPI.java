fe679a95fe032ebeabb3eaa45d98f1e36d509ca9
Rafael Rondão <rafael.rondao@gmail.com>
2018-02-13 22:21:27 -0200

    Improving TMDB API so it return query as Movie objects.
    Following is necessary so names with underscore (original_title)
    are parsed as camel case (originalTitle).
    
    .setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
    
    Signed-off-by: Rafael Rondão <rafael.rondao@gmail.com>

--- app/src/main/java/com/rondao/upopularmovies_s01/data/source/MoviesAPI.java
+++ app/src/main/java/com/rondao/upopularmovies_s01/data/source/MoviesAPI.java
@@ -2,11 +2,22 @@ package com.rondao.upopularmovies_s01.data.source;
 
 import android.net.Uri;
 
+import com.google.gson.FieldNamingPolicy;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.google.gson.reflect.TypeToken;
+import com.rondao.upopularmovies_s01.data.model.Movie;
+
 import java.io.IOException;
 import java.io.InputStream;
+import java.lang.reflect.Type;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class MoviesAPI {
@@ -19,7 +30,19 @@ public class MoviesAPI {
 
     private static final String API_KEY_PARAM = "api_key";
 
-    public static String queryMovies(String query) {
+    public static ArrayList<Movie> getMovies(String query) {
+        JsonObject jsonRoot = new JsonParser().parse(queryMovies(query)).getAsJsonObject();
+        JsonArray jsonResults = jsonRoot.getAsJsonArray("results");
+
+        Gson gson = new GsonBuilder()
+                .setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
+                .create();
+
+        Type listType = new TypeToken<ArrayList<Movie>>(){}.getType();
+        return gson.fromJson(jsonResults, listType);
+    }
+
+    private static String queryMovies(String query) {
         String jsonResult = "";
 
         Uri queryUri = Uri.parse(BASE_API_URL + query).buildUpon()
