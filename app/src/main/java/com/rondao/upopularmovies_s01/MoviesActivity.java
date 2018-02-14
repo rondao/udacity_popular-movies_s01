f42d0cc692ddd6e953d016ea050f34fe83431b1c
Rafael Rondão <rafael.rondao@gmail.com>
2018-02-14 16:32:16 -0200

    Adding RecyclerView to MoviesActivity and setting it with MoviesAdapter.
    Signed-off-by: Rafael Rondão <rafael.rondao@gmail.com>

--- app/src/main/java/com/rondao/upopularmovies_s01/MoviesActivity.java
+++ app/src/main/java/com/rondao/upopularmovies_s01/MoviesActivity.java
@@ -3,6 +3,8 @@ package com.rondao.upopularmovies_s01;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 import com.rondao.upopularmovies_s01.data.model.Movie;
 import com.rondao.upopularmovies_s01.data.source.MoviesAPI;
@@ -10,12 +12,21 @@ import com.rondao.upopularmovies_s01.data.source.MoviesAPI;
 import java.util.ArrayList;
 
 public class MoviesActivity extends AppCompatActivity {
+    private RecyclerView mRecyclerView;
     private MoviesAdapter mMoviesAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movies);
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.recyclerview_thumbnails);
+
+        mRecyclerView.setLayoutManager(new GridLayoutManager(this, 2));
+        mRecyclerView.setHasFixedSize(true);
+
+        mMoviesAdapter = new MoviesAdapter();
+        mRecyclerView.setAdapter(mMoviesAdapter);
 
         new FetchMoviesTask().execute(MoviesAPI.MOST_POPULAR);
     }
