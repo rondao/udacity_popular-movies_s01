6c7f9e19e67480b4afb313e3329d911ac990c932
Rafael Rondão <rafael.rondao@gmail.com>
2018-02-14 16:31:51 -0200

    Creating AsyncTask to fetch movies list.
    Signed-off-by: Rafael Rondão <rafael.rondao@gmail.com>

--- app/src/main/java/com/rondao/upopularmovies_s01/MoviesActivity.java
+++ app/src/main/java/com/rondao/upopularmovies_s01/MoviesActivity.java
@@ -1,13 +1,34 @@
 package com.rondao.upopularmovies_s01;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.rondao.upopularmovies_s01.data.model.Movie;
+import com.rondao.upopularmovies_s01.data.source.MoviesAPI;
+
+import java.util.ArrayList;
+
 public class MoviesActivity extends AppCompatActivity {
+    private MoviesAdapter mMoviesAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movies);
+
+        new FetchMoviesTask().execute(MoviesAPI.MOST_POPULAR);
+    }
+
+    class FetchMoviesTask extends AsyncTask<String, Void, ArrayList<Movie>> {
+        @Override
+        protected ArrayList<Movie> doInBackground(String... params) {
+            return MoviesAPI.getMovies(params[0]);
+        }
+
+        @Override
+        protected void onPostExecute(ArrayList<Movie> movies) {
+            mMoviesAdapter.seMoviesData(movies);
+        }
     }
 }
