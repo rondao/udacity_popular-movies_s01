e41ac77d0f449e3b76060159e7b3ce8d8175abc6
Rafael Rondão <rafael.rondao@gmail.com>
2018-03-04 20:22:00 -0300

    Creating DbHelper to work on movies.db.
    Signed-off-by: Rafael Rondão <rafael.rondao@gmail.com>

---
+++ app/src/main/java/com/rondao/upopularmovies/data/db/MoviesDbHelper.java
@@ -0,0 +1,33 @@
+package com.rondao.upopularmovies.data.db;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import com.rondao.upopularmovies.data.db.MovieContract.*;
+
+public class MoviesDbHelper extends SQLiteOpenHelper {
+
+    private static final String DATABASE_NAME = "movies.db";
+    private static final int DATABASE_VERSION = 1;
+
+    public MoviesDbHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        final String SQL_CREATE_MOVIES_TABLE = "CREATE TABLE " + MovieEntry.TABLE_NAME + " (" +
+                MovieEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
+                MovieEntry.COLUMN_NAME + " TEXT NOT NULL, " +
+                "); ";
+
+        db.execSQL(SQL_CREATE_MOVIES_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + MovieEntry.TABLE_NAME);
+        onCreate(db);
+    }
+}
