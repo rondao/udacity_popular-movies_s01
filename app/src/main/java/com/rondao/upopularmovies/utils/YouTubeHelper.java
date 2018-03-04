cad162e20a21657c36a9707a3f3befb5cac4e6f3
Rafael Rondão <rafael.rondao@gmail.com>
2018-03-04 19:54:04 -0300

    Improving YouTubeHelper to compose URIs to watch YouTube videos.
    Signed-off-by: Rafael Rondão <rafael.rondao@gmail.com>

--- app/src/main/java/com/rondao/upopularmovies/utils/YouTubeHelper.java
+++ app/src/main/java/com/rondao/upopularmovies/utils/YouTubeHelper.java
@@ -1,9 +1,24 @@
 package com.rondao.upopularmovies.utils;
 
+import android.net.Uri;
+
 public class YouTubeHelper {
     private static final String YOUTUBE_THUMBNAIL_URL = "https://img.youtube.com/vi/%s/0.jpg";
+    private static final String YOUTUBE_APP = "vnd.youtube:";
+    private static final String YOUTUBE_WATCH = "http://www.youtube.com/watch";
+    private static final String YOUTUBE_VIDEO_PARAM = "v";
 
     public static String getThumbnail(String key){
         return String.format(YOUTUBE_THUMBNAIL_URL, key);
+    }
+
+    public static Uri getWebUri(String key) {
+        return Uri.parse(YOUTUBE_WATCH).buildUpon()
+                .appendQueryParameter(YOUTUBE_VIDEO_PARAM, key)
+                .build();
+    }
+
+    public static Uri getAppUri(String key) {
+        return Uri.parse(YOUTUBE_APP + key).buildUpon().build();
     }
 }
