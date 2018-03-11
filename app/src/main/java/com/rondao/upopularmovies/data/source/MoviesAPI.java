c7268c2cf8fdfd1eaa0086817007288f73d23a42
Rafael Rondão <rafael.rondao@gmail.com>
2018-03-11 18:24:30 -0300

    Implementing Query for a single movie in MoviesAPI.
    Also making getGenericList more generic, so it can
    retrieve not only List of generics, but also single
    generic type.
    
    Signed-off-by: Rafael Rondão <rafael.rondao@gmail.com>

--- app/src/main/java/com/rondao/upopularmovies/data/source/MoviesAPI.java
+++ app/src/main/java/com/rondao/upopularmovies/data/source/MoviesAPI.java
@@ -20,6 +20,7 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class MoviesAPI {
@@ -30,39 +31,52 @@ public class MoviesAPI {
     public static final String MOST_POPULAR = "/movie/popular";
     public static final String TOP_RATED = "/movie/top_rated";
 
+    public static final String MOVIE = "/movie/%d";
     public static final String MOVIE_REVIEWS = "/movie/%d/reviews";
     public static final String MOVIE_TRAILERS = "/movie/%d/videos";
 
     private static final String API_KEY_PARAM = "api_key";
 
     public static ArrayList<Movie> getMovies(String query) {
-        Type listType = new TypeToken<ArrayList<Movie>>(){}.getType();
-        return (ArrayList<Movie>) getGenericList(queryMovies(query), listType);
+        return jsonToType(queryMovies(query), new TypeToken<ArrayList<Movie>>(){});
+    }
+
+    public static Movie getMovie(int movieId) {
+        Movie m = jsonToType(queryMovie(movieId), new TypeToken<Movie>(){});
+        return m;
     }
 
     public static ArrayList<Review> getMovieReviews(int movieId) {
-        Type listType = new TypeToken<ArrayList<Review>>(){}.getType();
-        return (ArrayList<Review>) getGenericList(queryMovieReviews(movieId), listType);
+        return jsonToType(queryMovieReviews(movieId), new TypeToken<ArrayList<Review>>(){});
     }
 
     public static ArrayList<Trailer> getMovieTrailers(int movieId) {
-        Type listType = new TypeToken<ArrayList<Trailer>>(){}.getType();
-        return (ArrayList<Trailer>) getGenericList(queryMovieTrailers(movieId), listType);
+        return jsonToType(queryMovieTrailers(movieId), new TypeToken<ArrayList<Trailer>>(){});
     }
 
-    private static ArrayList<?> getGenericList(String json, Type listType) {
+    private static <T> T jsonToType(String json, TypeToken<T> typeToken) {
         JsonObject jsonRoot = new JsonParser().parse(json).getAsJsonObject();
-        JsonArray jsonResults = jsonRoot.getAsJsonArray("results");
-
         Gson gson = new GsonBuilder()
                 .setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
                 .create();
 
-        return gson.fromJson(jsonResults, listType);
+        if (typeToken.getRawType().isAssignableFrom(ArrayList.class)) {
+            return gson.fromJson(jsonRoot.getAsJsonArray("results"), typeToken.getType());
+        } else {
+            return gson.fromJson(jsonRoot, typeToken.getType());
+        }
     }
 
     private static String queryMovies(String query) {
         Uri queryUri = Uri.parse(BASE_API_URL + query).buildUpon()
+                .appendQueryParameter(API_KEY_PARAM, TMDB.getKey())
+                .build();
+
+        return queryUri(queryUri);
+    }
+
+    private static String queryMovie(int movieId) {
+        Uri queryUri = Uri.parse(BASE_API_URL + String.format(MOVIE, movieId)).buildUpon()
                 .appendQueryParameter(API_KEY_PARAM, TMDB.getKey())
                 .build();
 
